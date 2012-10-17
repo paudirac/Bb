9a12e0ada5745b0bc0d1f29aba12d5e08ae3a571
Pau Cervera <pau.cervera@gmail.com>
2012-10-18 00:07:28 +0200

    plus and minus and naive stack

--- parser.py
+++ parser.py
@@ -8,6 +8,19 @@ Parser modules.
 [1, 2]
 """
 
+from collections import defaultdict
+
+# : + ( x y -- x ) x y opplus
+def bb_plus(stack):
+    last = stack.pop()
+    blast = stack.pop()
+    return stack + bb_stack(blast + last)
+
+def bb_minus(stack):
+    last = stack.pop()
+    blast = stack.pop()
+    return stack + bb_stack(blast - last)
+
 def tokens(text):
     """
     >>> tokens("a b")
@@ -31,8 +44,52 @@ def symbol(token):
     return int(token) if num_int.match(token) else token
 
 def parse(text):
-    return (symbol(token) for token in tokens(text))
+    return [symbol(token) for token in tokens(text)]
+
+def bb_read(prompt="Bb> "):
+    readed = raw_input(prompt).strip()
+    return parse(readed) if readed else None
+
+def bb_stack(value): return [value]
+
+def identity(s): return s
+
+operator = defaultdict(lambda : identity)
+operator['+'] = bb_plus
+operator['-'] = bb_minus
+
+def _eval(stack):
+    if stack:
+        if stack[len(stack) - 1] == '.':
+            stack.pop() # pops .
+            if not stack:
+                print "Can't evaluate empty stack"
+                return stack
+            op = stack.pop()
+            return operator[op](stack)
+        else:
+            return stack
+    else:
+        return []
+
+def bb_eval(stack, new_stack, env=None):
+    if new_stack:
+        return _eval(stack + new_stack)
+    else:
+        return _eval(stack)
+
+def bb_print(result):
+    for i, r in enumerate(result):
+        print i, r
+
+def repl(prompt="Bb> "):
+    stack = []
+    while True:
+        stack = bb_eval(stack, bb_read())
+        bb_print(stack)
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    # import doctest
+    # doctest.testmod()
+
+    repl()
